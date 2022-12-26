f1225d3a00bb7ff68df6be4d65d16b1309fbb322
VladimirVysochanskiy <shunkar78@mail.ru>
2022-12-26 18:51:27 +0300

    Add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,42 @@
+﻿//Напишите программу, которая принимает на вход число и 
+//выдает количество цифр в числе.
+
+int InputNumber(string text)
+{
+    System.Console.Write(text);
+    int number;
+    while (true)    
+    {
+        string? txt = (Console.ReadLine());
+        if (int.TryParse(txt, out number))
+        {
+             break;
+        }
+        System.Console.Write("Введенное значение не является числом. Попробуйте ещё раз: ");
+    }  
+    return number;
+}
+
+int NumberOfDigit(int number)
+{
+    int count = 0;
+    number = Math.Abs(number);
+    if (number == 0)
+    {
+        count = 1;
+    }
+    else
+    {
+        while (number > 0)
+        {
+            count++;
+            number /= 10;
+        }
+    }
+    return count;
+}
+
+int N = InputNumber("Введите число: ");
+int count = NumberOfDigit(N);
+System.Console.WriteLine(count);
+
