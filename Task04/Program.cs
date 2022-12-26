2059ef8408f85173e513c56b22b298f980226655
VladimirVysochanskiy <shunkar78@mail.ru>
2022-12-26 20:11:23 +0300

    Add Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая выводит массив из 8 элементов, 
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0] */
+
+int[] K = RandArray(8, 0, 1);
+for (int i = 0; i < K.Length; i++)
+{
+    System.Console.Write($"{K[i]}, ");
+}
+
+
+int[] RandArray(int length, int fromNum, int toNum)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(fromNum, toNum + 1);
+    }
+    return array;
+}
